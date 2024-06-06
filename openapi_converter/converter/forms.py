e04a518698b09671b9de3285c3abb67ddacb3d44
samplecodeshare <119216481+samplecodeshare@users.noreply.github.com>
2024-06-06 19:21:29 -0400

    Update forms.py

--- openapi_converter/converter/forms.py
+++ openapi_converter/converter/forms.py
@@ -8,3 +8,15 @@ class InputForm(forms.Form):
     ]
     input_type = forms.ChoiceField(choices=INPUT_TYPES, required=True)
     content = forms.CharField(widget=forms.Textarea, required=True)
+
+class OpenAPIGeneratorForm(forms.Form):
+    INPUT_CHOICES = [
+        ('json', 'Sample JSON'),
+        ('sql', 'SQL Query'),
+    ]
+    input_type = forms.ChoiceField(choices=INPUT_CHOICES, required=True)
+    json_sample = forms.CharField(widget=forms.Textarea, required=False)
+    connection_string = forms.CharField(required=False)
+    user_id = forms.CharField(required=False)
+    password = forms.CharField(widget=forms.PasswordInput, required=False)
+    sql_query = forms.CharField(widget=forms.Textarea, required=False)
